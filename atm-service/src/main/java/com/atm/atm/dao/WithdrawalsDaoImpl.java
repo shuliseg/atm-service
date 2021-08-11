559ac974e4ff2abebdf0a02ab6f0b148cdb8b052
shuli_se <shulanit_se@malam.com>
2021-08-12 01:44:54 +0300

    add queries

--- atm-service/src/main/java/com/atm/atm/dao/WithdrawalsDaoImpl.java
+++ atm-service/src/main/java/com/atm/atm/dao/WithdrawalsDaoImpl.java
@@ -16,32 +16,30 @@ public class WithdrawalsDaoImpl implements WithdrawalsDao{
 
     @Override
     public int getWithdrawalsCountByCreditCardAndSecretCode(WithdrawalRequest withdrawalRequest) throws WithdrawalException {
-//        try {
-//            StringBuilder hql = new StringBuilder();
-//            hql.append("select count(*) from WITHDRAWALS where CARD_NUMBER =:cardNumber and WITHDRAWAL_DATE = CURRENT DATE");
-//            Query query = createQuery(hql);
-//            query.setParameter("cardNumber", withdrawalRequest.getCardNumber());
-//            return query.getFirstResult();
-//        } catch (Exception e) {
-//            logger.error(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_COUNT.toString());
-//            throw new WithdrawalException(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_COUNT.toString());
-//        }
-        return 2;
+        try {
+            StringBuilder hql = new StringBuilder();
+            hql.append("select count(*) from WITHDRAWALS where CARD_NUMBER =:cardNumber and WITHDRAWAL_DATE = CURRENT DATE");
+            Query query = createQuery(hql);
+            query.setParameter("cardNumber", withdrawalRequest.getCardNumber());
+            return query.getFirstResult();
+        } catch (Exception e) {
+            logger.error(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_COUNT.toString());
+            throw new WithdrawalException(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_COUNT.toString());
+        }
     }
 
     @Override
     public Double getWithdrawalsSumByCreditCardAndSecretCode(WithdrawalRequest withdrawalRequest) throws WithdrawalException {
-//        try {
-//            StringBuilder hql = new StringBuilder();
-//            hql.append("select sum(amount) from WITHDRAWALS where CARD_NUMBER =:cardNumber and WITHDRAWAL_DATE = CURRENT DATE");
-//            Query query = createQuery(hql);
-//            query.setParameter("cardNumber", withdrawalRequest.getCardNumber());
-//            return Double.valueOf(query.getFirstResult());
-//        } catch (Exception e) {
-//            logger.error(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_SUM.toString());
-//            throw new WithdrawalException(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_SUM.toString());
-//        }
-        return 3d;
+        try {
+            StringBuilder hql = new StringBuilder();
+            hql.append("select sum(amount) from WITHDRAWALS where CARD_NUMBER =:cardNumber and WITHDRAWAL_DATE = CURRENT DATE");
+            Query query = createQuery(hql);
+            query.setParameter("cardNumber", withdrawalRequest.getCardNumber());
+            return Double.valueOf(query.getFirstResult());
+        } catch (Exception e) {
+            logger.error(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_SUM.toString());
+            throw new WithdrawalException(ErrorMsgEnum.ERROR_IN_GET_WITHDRAWALS_SUM.toString());
+        }
     }
 
     @Override
